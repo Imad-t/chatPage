ee85e7df46aead2a4baf4c2312a97cc6d663cad3
Imad-t <imadeddineterraf@gmail.com>
2023-06-18 02:07:47 +0100

    header=>Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import ChatBody from "./ChatBody";
 import ChatList from "./ChatList";
-import Header from "./header";
+import Header from "./Header";
 import styles from "./styles/Chat.module.scss";
-import { useState } from 'react';
+import { useState } from "react";
 
 const ChatPage = () => {
   const [selectedConversation, setSelectedConversation] = useState(null);
 
   return (
     <div className={styles.app}>
-    <Header className={styles.header}/>
+      <Header className={styles.header} />
       <div className={styles.chat}>
-        <ChatList setSelectedConversation={setSelectedConversation}  className={styles.item}/>
-        <ChatBody selectedConversation={selectedConversation} className={styles.item}/>
+        <ChatList
+          setSelectedConversation={setSelectedConversation}
+          className={styles.item}
+        />
+        <ChatBody
+          selectedConversation={selectedConversation}
+          className={styles.item}
+        />
       </div>
     </div>
   );
